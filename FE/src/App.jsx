82c1d4963e4075efe07dda1fd36df79c2e7f2061
Tara Sedovšek <tara.sedovsek@student.um.si>
2026-05-11 13:56:37 +0200

    .

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 function App() {
   const [status, setStatus] = useState('')
+  const [podjetja, setPodjetja] = useState([])
 
   const API_URL = 'https://povezava-si.onrender.com'
 
@@ -11,14 +12,23 @@ function App() {
       .then(res => res.json())
       .then(data => setStatus(data.message))
       .catch(err => setStatus('BE ne odgovarja'))
+
+    fetch(`${API_URL}/api/podjetja`)
+      .then(res => res.json())
+      .then(data => setPodjetja(data))
+      .catch(err => console.error('Napaka:', err))
   }, [])
 
   return (
-    <ul>
-  {podjetja.map(p => (
-    <li key={p.maticna}>{p.popolno_ime} — {p.posta}</li>
-  ))}
-</ul>
+    <div>
+      <h1>Povezava.si</h1>
+      <p>Backend status: {status}</p>
+      <ul>
+        {podjetja.map(p => (
+          <li key={p.maticna}>{p.popolno_ime} — {p.posta}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
