3245b04ad4db3a17023bddc1a61838943dfc1c4b
Tara Sedovšek <tara.sedovsek@student.um.si>
2026-05-11 13:37:01 +0200

    .

--- BE/server.js
+++ BE/server.js
@@ -5,10 +5,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  next()
+})
 
 
 app.get('/', (req, res) => {
