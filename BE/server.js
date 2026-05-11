acccef19367780db18ac4d1060fabbda1f921802
Tara Sedovšek <tara.sedovsek@student.um.si>
2026-05-11 13:52:54 +0200

    test baze na fe

--- BE/server.js
+++ BE/server.js
@@ -8,6 +8,11 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// Routes
+const podjetjaRouter = require('./routes/podjetja')
+app.use('/api/podjetja', podjetjaRouter)
+
+
 app.get('/', (req, res) => {
   res.json({ status: 'ok', message: 'Povezava.si backend deluje!' });
 });
