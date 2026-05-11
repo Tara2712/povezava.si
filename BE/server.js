b6a7797dc5edda290578de6fb756b128407cbfff
Tara Sedovšek <tara.sedovsek@student.um.si>
2026-05-11 18:51:43 +0200

    dodajanje testnega endpointa

--- BE/server.js
+++ BE/server.js
@@ -8,10 +8,20 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// endpointi
 const podjetjaRouter = require('./routes/podjetja')
 app.use('/api/podjetja', podjetjaRouter)
 
+app.get('/api/test', (req, res) => { // test, če baza dela na renderju
+  res.json({ message: 'Test endpoint deluje' })
+});
+//test verzije na renderju
+app.get('/api/version', (req, res) => {
+  res.json({
+    version: '2026-05-11-1',
+    message: 'Render uporablja novo verzijo server.js'
+  })
+})
 
 app.get('/', (req, res) => {
   res.json({ status: 'ok', message: 'Povezava.si backend deluje!' });
--- BE/testDb.js
+++ BE/testDb.js
@@ -11,8 +11,27 @@ const pool = new Pool({
 async function test() {
   try {
     console.log('Poskušam se povezati...')
-    const result = await pool.query('SELECT NOW()')
-    console.log('Povezava uspešna! Čas:', result.rows[0].now)
+
+    const timeResult = await pool.query('SELECT NOW()')
+    console.log('Povezava uspešna! Čas:', timeResult.rows[0].now)
+
+    const tableResult = await pool.query(`
+      SELECT COUNT(*) 
+      FROM podjetja
+    `)
+
+    console.log('Število podjetij v tabeli:', tableResult.rows[0].count)
+
+    //dela!!!
+    const sampleResult = await pool.query(`
+      SELECT maticna, popolno_ime, posta, pravna_oblika
+      FROM podjetja
+      LIMIT 5
+    `)
+
+    console.log('Primeri podjetij:')
+    console.table(sampleResult.rows)
+
   } catch (err) {
     console.error('Napaka:', err.message)
   } finally {
